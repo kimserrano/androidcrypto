997e15589c19c5d670d368109661e554620e75d5
kimserrano <alejandra.ser.ur@gmail.com>
2024-02-29 14:40:23 -0600

    hola

---
+++ app/src/main/java/serrano/kimberly/androidcrypto/MainActivity.kt
@@ -0,0 +1,94 @@
+package serrano.kimberly.androidcrypto
+
+
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import serrano.kimberly.androidcrypto.ui.theme.AndroidcryptoTheme
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+@RequiresApi(Build.VERSION_CODES.M)
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val cryptoManager = CryptoManager()
+        setContent {
+            AndroidcryptoTheme {
+                var messageToEncrypt by remember {
+                    mutableStateOf("")
+                }
+                var messageToDecrypt by remember {
+                    mutableStateOf("")
+                }
+                Column(
+                    modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                ) {
+                    TextField(
+                        value = messageToEncrypt,
+                        onValueChange = { messageToEncrypt = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text(text = "Encrypt string") }
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(onClick = {
+                            val bytes = messageToEncrypt.encodeToByteArray()
+                            val file = File(filesDir, "secret.txt")
+                            if (!file.exists()) {
+                                file.createNewFile()
+                            }
+                            val fos = FileOutputStream(file)
+
+                            messageToDecrypt = cryptoManager.encrypt(
+                                bytes = bytes,
+                                outputStream = fos
+                            ).decodeToString()
+                        }) {
+                            Text(text = "Encrypt")
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            val file = File(filesDir, "secret.txt")
+                            messageToEncrypt = cryptoManager.decrypt(
+                                inputStream = FileInputStream(file)
+                            ).decodeToString()
+                        }) {
+                            Text(text = "Decrypt")
+                        }
+                    }
+                    Text(text = messageToDecrypt)
+                }
+            }
+        }
+    }
+}
